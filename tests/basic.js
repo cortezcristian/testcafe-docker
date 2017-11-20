347c6f8c7305b632c98cd93057772c3ae752e8f2
Achim Rolle <info@achimrolle.de>
2017-11-20 19:44:07 +0100

    add window size to container, maximize window before each test

--- tests/basic.js
+++ tests/basic.js
@@ -2,10 +2,13 @@ import { Selector } from 'testcafe'
 
 fixture `Getting Started`
   .page `http://devexpress.github.io/testcafe/example`
+  .beforeEach( async t => {
+    await t.maximizeWindow()
+})
 
 test('My first test', async t => {
   await t
     .typeText('#developer-name', 'John Smith')
-    .takeScreenshot()
+    .takeScreenshot('test.png')
     .click('#submit-button')
 })
