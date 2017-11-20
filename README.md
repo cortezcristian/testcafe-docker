7aa3bdb9d931f4d81df90b596a0ca039d5a823be
Achim Rolle <info@achimrolle.de>
2017-11-20 19:10:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# testcafe-docker
+
+> Dockerized testcafe project
+
+Run `docker-compose up -d` to start the docker container and run the tests
---
+++ tests/basic.js
@@ -0,0 +1,11 @@
+import { Selector } from 'testcafe'
+
+fixture `Getting Started`
+  .page `http://devexpress.github.io/testcafe/example`
+
+test('My first test', async t => {
+  await t
+    .typeText('#developer-name', 'John Smith')
+    .takeScreenshot()
+    .click('#submit-button')
+})
